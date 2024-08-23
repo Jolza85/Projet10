e85bfe7190e32b7745b84b4e3b56592a2e82fba4
Jolza85 <leo.chaigneau@orange.fr>
2024-08-23 13:03:47 +0200

    update app streamlit + add seaborn on requirment.txt

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import shap
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+
+# Définir une palette de couleurs
+color_map = {
+    '0': '#636EFA',  # Couleur pour le Cluster 0
+    '1': '#EF553B',  # Couleur pour le Cluster 1
+    '2': '#00CC96',  # Couleur pour le Cluster 2
+    '3': '#AB63FA',  # Couleur pour le Cluster 3
+    '4': '#FFA15A',  # Couleur pour le Cluster 4
+}
 
 # Fonction pour charger les données en fonction du modèle sélectionné
 def load_data(model_name):
@@ -23,7 +35,7 @@ st.title("Dashboard classification photos restaurants avec CLIP d'OpenAI")
 
 # Afficher les images des classes
 classes = ["drink", "food", "inside", "menu", "outside"]
-images = ["drink.jpg", "food.jpg", "inside.jpg", "menu.jpg", "outside.jpg"]  # Remplacez par les chemins réels de vos images
+images = ["drink.jpg", "food.jpg", "inside.jpg", "menu.jpg", "outside.jpg"] 
 
 st.subheader('Représentation des classes')
 cols = st.columns(5)
@@ -43,6 +55,10 @@ df_embeddings = load_data(model_name)
 if df_embeddings.empty:
     st.write("Aucune donnée disponible pour le modèle sélectionné.")
 else:
+    # Encodage des labels de classes en numériques
+    le = LabelEncoder()
+    df_embeddings['num_class'] = le.fit_transform(df_embeddings['class'])
+    
     # Créer un graphique interactif en 3D avec couleurs par classe
     fig_3d = px.scatter_3d(
         df_embeddings, x='tsne1', y='tsne2', z='tsne3',
@@ -62,20 +78,41 @@ else:
 
     st.subheader(f"Visualisation 2D t-SNE des Embeddings - {model_name}")
     st.plotly_chart(fig_2d)
-
-    # Afficher le DataFrame
-    st.subheader("Données t-SNE")
-    st.dataframe(df_embeddings)
     
     # Calcul des clusters (KMeans)
     X_tsne = df_embeddings[['tsne1', 'tsne2', 'tsne3']].values
     cls_model = KMeans(n_clusters=5, random_state=6)
     df_embeddings['cluster'] = cls_model.fit_predict(X_tsne)
 
+    st.subheader(f"Visualisation 2D t-SNE des Clusters Prédits - {model_name}")
+    fig_2d_clusters = px.scatter(
+        df_embeddings, x='tsne1', y='tsne2',
+        color=df_embeddings['cluster'].astype(str),  # Convertir les clusters en string pour le mapping des couleurs
+        color_discrete_map=color_map,  # Appliquer la palette de couleurs
+        labels={'tsne1': 'TSNE1', 'tsne2': 'TSNE2'}
+    )
+    st.plotly_chart(fig_2d_clusters)
+
+    # Afficher le DataFrame
+    st.subheader("Données T-SNE avec la class initiale et le cluster prédit")
+    st.dataframe(df_embeddings)
+    
     # Calculer le score ARI
     ari_score = adjusted_rand_score(df_embeddings['class'], df_embeddings['cluster'])
     
     st.subheader(f"Score ARI : {ari_score:.3f}")
+
+    # Calcul de la matrice de confusion
+    conf_mat = metrics.confusion_matrix(df_embeddings['num_class'], df_embeddings['cluster'])
+
+    # Création du DataFrame pour la heatmap
+    df_cm = pd.DataFrame(conf_mat, index=[label for label in classes], columns=[i for i in range(5)])
+
+    # Affichage de la heatmap dans Streamlit
+    st.subheader("Heatmap entre les Clusters et les Catégories des Images")
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(df_cm, annot=True, cmap="Blues", fmt='d')
+    st.pyplot(plt)
     
     # Fonction pour prédire les distances aux centres des clusters
     def predict_cluster_distances(data):
